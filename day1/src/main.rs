39ee2edfa018cac66e220a5445e562eca4684dfa
Adam Sheehan <a.t.sheehan@gmail.com>
2023-12-03 15:03:21 -0500

    Day 1 part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = read_to_string("input/test").unwrap();
+    let sum = sum_of_calibration_values(&contents);
+    println!("The sum of the calibration values is {}", sum);
+}
+
+fn sum_of_calibration_values(contents: &str) -> u32 {
+    contents.lines().map(parse_calibration_value).sum()
+}
+
+fn parse_calibration_value(line: &str) -> u32 {
+    let digits: Vec<char> = line.chars().filter(|c| c.is_ascii_digit()).collect();
+    let number = format!("{}{}", digits.first().unwrap(), digits.last().unwrap());
+    number.parse::<u32>().unwrap()
 }
